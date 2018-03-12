8789df5eed083ff24528b4f90bea7366f50a2974
Madlenne <33123587+Madlenne@users.noreply.github.com>
2018-03-12 19:30:37 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -21,3 +21,51 @@
 #
 #Your program must be runnable with command "python task.py".
 #Show some usecases of your library in the code (print some things)
+
+class classDiary:
+
+	def __init__(self, name="noName", surname="noName", grade=1.0, attendance = 0):
+		self.number_of_students=0;
+		self.cDiary = {}
+		self.cDiary['{} {}'.format(name, surname)] = [grade, attendance]
+
+	def print_class_diary(self):
+		print ("This is the diary of the class:")
+		print ("All the information is in the following order:")
+		print ("name and surname, grade, attendance")
+		print (self.cDiary)
+
+	def add_new_student(self, name, surname, grade, attendance):
+		self.cDiary.update({'{} {}'.format(name, surname) : [grade, attendance]})
+		self.number_of_students+=1
+
+	def average(self):
+		self.sum = 0.0
+
+		for x in range(self.number_of_students):
+			self.sum += self.cDiary[x][0]
+		self.av = self.sum / self.number_of_students
+		return self.av
+
+	def totalAttendance(self):
+		self.attendance_sum = 0
+
+		for x in range(self.number_of_students):
+			self.attendance_sum += self.cDiary[x][1]
+		return self.attendance_sum
+
+
+
+
+diary = classDiary("Ann", "Smith", 3.0, 2)
+diary.print_class_diary()
+
+diary.add_new_student("Tom", "Baker", 4.5, 5)
+diary.print_class_diary()
+
+
+
+
+
+
+
